53459df75744f142c23e56659d584be8515a25f2
Dimonasdf <dimonasdf@inbox.ru>
2018-09-26 00:48:02 +0300

    asteroids initial commit

---
+++ games/the_project/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1505226945,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tilemap.png",
+		"images/collright.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
